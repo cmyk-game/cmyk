f8f8041f34747b2d28839ec6525dea2a4e86c59b
Ethan Kennerly <ethan@lumoslabs.com>
2014-01-25 18:48:51 -0800

    Materials distinguish buttons.

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ var playerMaterials = [
   colorConverter.from_cmyk({ c: 000, m: 000, y: 100, k: 000 }).hex(),
 ]
 
+
+
+
+
+
 var gameMaterials = [
   // materials start at index 1 (since 0 is empty space)
   colorConverter.from_cmyk({ c: 000, m: 000, y: 000, k: 075 }).hex(),
@@ -18,7 +23,14 @@ var gameMaterials = [
   colorConverter.from_cmyk({ c: 000, m: 100, y: 000, k: 000 }).hex(),
   colorConverter.from_cmyk({ c: 000, m: 000, y: 100, k: 000 }).hex(),
   colorConverter.from_cmyk({ c: 100, m: 100, y: 000, k: 000 }).hex(),
+  colorConverter.from_cmyk({ c: 000, m: 000, y: 000, k: 100 }).hex(),
+  colorConverter.from_cmyk({ c: 100, m: 000, y: 000, k: 000 }).hex(),
+  colorConverter.from_cmyk({ c: 000, m: 100, y: 000, k: 000 }).hex(),
+  colorConverter.from_cmyk({ c: 000, m: 000, y: 100, k: 000 }).hex(),
+  colorConverter.from_cmyk({ c: 100, m: 100, y: 000, k: 000 }).hex(),
+  colorConverter.from_cmyk({ c: 100, m: 100, y: 100, k: 000 }).hex(),
   colorConverter.from_cmyk({ c: 000, m: 100, y: 100, k: 000 }).hex(),
+  colorConverter.from_cmyk({ c: 050, m: 000, y: 050, k: 000 }).hex(),
   colorConverter.from_cmyk({ c: 100, m: 000, y: 100, k: 000 }).hex(),
 ]
 

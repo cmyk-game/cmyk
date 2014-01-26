d70c1617e121d41c06c19aaebb8cf8dbe3f5b1f7
Xiaohan Zhang <hellocharlien@hotmail.com>
2014-01-25 23:13:45 -0800

    textures

--- index.js
+++ index.js
@@ -12,28 +12,32 @@ var playerMaterials = [
   colorConverter.from_cmyk({ c: 000, m: 000, y: 100, k: 000 }).hex(),
 ]
 
-
-
-
-
-
 var gameMaterials = [
   // materials start at index 1 (since 0 is empty space)
-  colorConverter.from_cmyk({ c: 000, m: 000, y: 000, k: 075 }).hex(),
-  colorConverter.from_cmyk({ c: 100, m: 000, y: 000, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 000, m: 100, y: 000, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 000, m: 000, y: 100, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 100, m: 100, y: 000, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 000, m: 000, y: 000, k: 100 }).hex(),
-  colorConverter.from_cmyk({ c: 100, m: 000, y: 000, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 000, m: 100, y: 000, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 000, m: 000, y: 100, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 100, m: 100, y: 000, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 100, m: 100, y: 100, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 000, m: 100, y: 100, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 050, m: 000, y: 050, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 100, m: 000, y: 100, k: 000 }).hex(),
-  colorConverter.from_cmyk({ c: 000, m: 000, y: 000, k: 075 }).hex(),
+  "stone_brick",
+
+  // doors, 2-6
+  "solid_color/blue_solid",
+  "solid_color/red_solid",
+  "solid_color/yellow_solid",
+  "solid_color/orange_solid",
+  "solid_color/black_solid",  //blocks everyone
+
+  //buttons, 7-11
+  "button_blue",
+  "button_red",
+  "button_yellow",
+  "button_orange",
+  "button_black", //activatable by everyone
+
+  //wire, 12
+  "wires/wire_prism",
+
+  //start, 13
+  "deco_tile_01",
+
+  //end, 14
+  "black_stone_brick",
 ]
 
 // choose a player color
